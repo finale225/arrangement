e161746a6abdc3a7b1774497b68a27e9bd578a38
finale225 <finale2255@naver.com>
2021-06-23 15:38:00 +0900

    max

--- main.cpp
+++ main.cpp
@@ -1,35 +1,20 @@
-/*#include <iostream>
-using namespace std;
-
-int main() {
-    int n, max, min;
-    cin >> n;
-    int* a = new int [n];
-    cin >> a[0];
-    max = min = a[0];
-    for (int i=1; i<n; i++) {
-        cin >> a[i];
-        if (a[i] >= max) max = a[i];
-        else if (a[i] < min) min = a[i];
-    }
-    cout << min << ' ' << max;
-    delete [] a;
-}*/
-
 #include <iostream>
 using namespace std;
 
 int main() {
-    int n;
-    int max = -1000000;
-    int min = 1000000;
-    cin >> n;
-    if (n<1) return 0;
-    int a[n];
-    for(int i=0; i<n; i++) {
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    int a[9], count = 0;
+    cin >> a[0];
+    int max = a[0];
+    for (int i=1; i<9; i++) {
         cin >> a[i];
         if (a[i] > max) max = a[i];
-        if (a[i] < min) min = a[i];
     }
-    cout << min << ' ' << max;
+    for (int i=0; i<9; i++) {
+        count++;
+        if (a[i] == max) break;
+    }
+    cout << max << '\n' << count;
 }
