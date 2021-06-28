7e0a2c2a7be999b5661bbd56d1ac0f39caeab03a
finale225 <finale2255@naver.com>
2021-06-28 13:48:48 +0900

    count0to9

--- main.cpp
+++ main.cpp
@@ -5,16 +5,21 @@ int main() {
     cin.tie(NULL);
     cout.tie(NULL);
     ios_base::sync_with_stdio(false);
-    int a[9], count = 0;
-    cin >> a[0];
-    int max = a[0];
-    for (int i=1; i<9; i++) {
-        cin >> a[i];
-        if (a[i] > max) max = a[i];
+    int a, b, c, result, divN;
+    int n[10] = {0, }; // [0]은 0의 개수, [1]은 1의 개수, ... , [9]는 9의 개수
+    cin >> a >> b >> c; // 세 가지 수 입력받기
+    result = a*b*c; // result는 세 가지 수의 곱
+    if (result >= 100000000) divN = 1000000000;
+    else if (result >= 10000000) divN = 100000000;
+    else divN = 10000000;
+    while (true) {
+        divN /= 10;
+        int d = result / divN;
+        for (int i=0; i<10; i++) {
+            if (i==d) n[i]++;
+        }
+        result %= divN;
+        if (divN == 1) break;
     }
-    for (int i=0; i<9; i++) {
-        count++;
-        if (a[i] == max) break;
-    }
-    cout << max << '\n' << count;
+    for (int i=0; i<10; i++) cout << n[i] << '\n';
 }
