a4a82ccf57c5d59c2b9363e82026109490790ab7
finale225 <86174673+finale225@users.noreply.github.com>
2021-06-28 18:05:40 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#define stdN 42
 using namespace std;
 
 int main() {
@@ -8,15 +7,15 @@ int main() {
     int n, max = 0;
     double sum = 0;
     cin >> n;
-    double arr[n];
+    double arr[n]; // 크기가 입력받은 수인 배열을 생성
     for (int i=0; i<n; i++) {
-        cin >> arr[i];
-        if (arr[i] > max) max = arr[i];
+        cin >> arr[i]; // 배열에 원래의 점수 값을 입력
+        if (arr[i] > max) max = arr[i]; // 최대값 산출
     }
     for (int i=0; i<n; i++) {
-        arr[i] = arr[i]*100 / max;
-        sum += arr[i];
+        arr[i] = arr[i]*100 / max; // 조작된 점수로 배열을 다시 초기화
+        sum += arr[i]; // 조작된 점수의 합으로 sum 초기화
     }
-    cout.precision(8);
-    cout << sum / n;
+    cout.precision(8); // 8자리의 숫자로 표현.
+    cout << sum / n; // 조작된 점수의 합 평균
 }
